27dd210cc7100168013d8caabed46cdf21fd88bb
narendrarane50 <68918256+narendrarane50@users.noreply.github.com>
2023-02-03 21:46:53 +0530

    implemented creating AI image

--- server/index.js
+++ server/index.js
@@ -1,26 +1,33 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import connectDB from './mongodb/connect.js'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
-dotenv.config()
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
-const app = express()
-app.use(cors())
-app.use(express.json({limit: '50mb'}))
+dotenv.config();
 
-app.get('/',async (req,res) => {
-    res.send('Hello from DALL-E')
-})
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 
-const startServer = async () =>{
-    try {
-        connectDB(process.env.MONGODB_URL)
-        app.listen(8080,() => console.log('Server has started on port http://localhost:8080'))
-    } catch (error) {
-        console.log(error)
-    }
-    
-}
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
-startServer()
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
